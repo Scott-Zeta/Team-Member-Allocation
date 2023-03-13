2bd3ca1787825d7642a72d59a895f565c627a6d0
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-13 07:25:45 +0000

    The grid of dashboard name list

--- src/DashBorad.jsx
+++ src/DashBorad.jsx
@@ -1,15 +1,16 @@
-const Dashboard = ({selectedTeam, countMember,employees}) => {
-  return(
+const Dashboard = ({ selectedTeam, countMember, employees }) => {
+  return (
     <>
       <h4>{selectedTeam} now has {countMember} members.</h4>
-      <h4 style={{color:'skyblue'}}>There are x people currently not been assigned to any team</h4>
-      <div>
+      <h4 style={{ color: 'skyblue' }}>People currently not been assigned to any team</h4>
+      <div className="card-collection">
         {employees.map((e) => {
-          return e.teamName === ''?<p key={e.id}>{e.fullName}</p>:<div key={e.id}></div>})
+          return e.teamName === '' ? <p key={e.id}>{e.fullName}</p> : null
+        })
         }
       </div>
-      <h4 style={{color:'gold'}}>There are x people currently been assigned more than 2 team</h4>
-      <div>{}</div>
+      <h4 style={{ color: 'gold' }}>People currently been assigned more than 2 team</h4>
+      <div className="card-collection">{}</div>
     </>
   )
 }
