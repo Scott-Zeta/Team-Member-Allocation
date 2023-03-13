c3c528c6e7ff720427d64d5f925b2aadc14e95a1
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-13 03:24:43 +0000

    implement nav bar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Nav = () =>{
   return(
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/'>Home</Link>
         </li>
-        <li>
-          <Link to='/groupMember'>Teams</Link>
+        <li className='nav-item'>
+          <Link className='nav-link' to='/groupMember'>Teams</Link>
         </li>
       </ul>
     </nav>
