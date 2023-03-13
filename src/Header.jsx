44d97eb62fe6283a95ba7379004e10c578d3da4a
Scott Zeta <100838867+Scott-Zeta@users.noreply.github.com>
2023-03-13 01:51:47 +0000

    bootstrap css on header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,12 @@
 const header = ({selectedTeam,countMember}) => {
   return (
-    <header>
-      <h1>Team Member Allocation</h1>
-      <h3>{selectedTeam} now has {countMember} members.</h3>
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1>Team Member Allocation</h1>
+          <h3>{selectedTeam} now has {countMember} members.</h3>
+        </div>
+      </div>
     </header>
   )
 }
